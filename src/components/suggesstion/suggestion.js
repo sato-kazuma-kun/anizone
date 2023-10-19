7f603af1f91016e541ff1372d7070270f1e2d50e
kazuma-aniflex <145659895+kazuma-aniflex@users.noreply.github.com>
2023-10-19 14:32:48 +0530

    Update suggestion.js

--- src/components/suggesstion/suggestion.js
+++ src/components/suggesstion/suggestion.js
@@ -1,7 +1,104 @@
+// 'use client'
+
+// import styles from './suggesstion.module.css'
+// import { animeDataAPI } from '@/api/api'
+// import Link from 'next/link'
+// import React, { useEffect, useState } from 'react'
+// import ErrorComponent from '@/components/error/error'
+// import Loading from '@/app/loading'
+// import Image from 'next/image'
+// import HeadingComponent from '@/components/heading/heading'
+
+// export default function Suggesstion({ title, link, link_title }) {
+//     const [datas, setDatas] = useState(null)
+//     const [isLoading, setIsLoading] = useState(true)
+//     const [error, setError] = useState(null)
+
+//     async function fetchAnimeDataAPI() {
+//         try {
+//             setIsLoading(true) // Set isLoading to true before the API call
+//             setError(null)
+
+//             const fetchedData = await animeDataAPI()
+//             setDatas(fetchedData)
+
+//             setIsLoading(false)
+//         } catch (error) {
+//             console.error('Failed to fetch recently updated data:', error)
+//             setError('Failed to fetch recently updated data. Please try again later.')
+
+//             setIsLoading(false)
+//         }
+//     }
+
+//     useEffect(() => {
+//         fetchAnimeDataAPI()
+//     }, [])
+
+//     async function retryFetchingSuggesstion() {
+//         setError(null)
+//         setIsLoading(true)
+
+//         await new Promise((resolve) => setTimeout(resolve, 500))
+//         fetchAnimeDataAPI()
+
+//         setIsLoading(false)
+//     }
+
+//     return (
+//         <section id='suggesstion' className={styles.container}>
+
+//             <HeadingComponent title={title} link={link} link_title={link_title} />
+
+//             {/* On error it'll render this element */}
+//             {error && <ErrorComponent error={error} onRetryClick={retryFetchingSuggesstion} />}
+
+//             {/* If loading it'll render this element */}
+//             {isLoading && <Loading />}
+
+//             {/* If not loading and there are no error then it'll render this element */}
+//             {!error && !isLoading && (
+//                 <>
+//                     <div id="content-container" className={styles.contentContainer}>
+//                         {datas?.map(data => (
+//                             <div key={data.title} className={styles.suggestionDiv}>
+//                                 <Link href={`/view/${data.anime}`}>
+//                                     <span className={styles.flexSpan}>
+//                                         <Image alt={data.title} width={300} height={168} className={styles.newStyleBackground} src={data['cover-image'][0]['landscape']} />
+//                                     </span>
+//                                     <div className={styles.newStyleMain}>
+//                                         <span style={{ width: '50%', paddingLeft: '.5rem' }}>
+//                                             <Image alt={data.title} width={150} height={225} className={styles.newStyleForeground} src={data['cover-image'][0]['portrait']} />
+//                                         </span>
+//                                         <span style={{ width: '50%', paddingLeft: '.5rem', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+//                                             <p className={styles.newStyleText}>
+//                                                 <b>{data.title}</b>
+//                                                 <br />
+//                                             </p>
+//                                             <span
+//                                                 className={styles.newStyleType}
+//                                                 style={{ color: data.type === 'Movie' ? '#2abdbb' : '#f47521' }}
+//                                             >
+//                                                 {data.type}
+//                                             </span>
+
+//                                         </span>
+//                                     </div>
+//                                 </Link>
+//                             </div>
+//                         ))}
+//                     </div>
+//                 </>
+//             )}
+//         </section>
+//     )
+// }
+
 'use client'
 
 import styles from './suggesstion.module.css'
-import { animeDataAPI } from '@/api/api'
+// import { animeDataAPI } from '@/api/api'
+import { calcUserPref } from '@/algorithm/anime-suggestion'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import ErrorComponent from '@/components/error/error'
@@ -19,8 +116,8 @@ export default function Suggesstion({ title, link, link_title }) {
             setIsLoading(true) // Set isLoading to true before the API call
             setError(null)
 
-            const fetchedData = await animeDataAPI()
-            setDatas(fetchedData)
+            const suggestions = await calcUserPref()
+            setDatas(suggestions)
 
             setIsLoading(false)
         } catch (error) {
